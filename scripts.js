e9e4f772bda0ca92a179726f2b677ceaa74e0e24
Peter André Altier <petera113.pa@gmail.com>
2024-11-25 10:13:58 +0100

    about me

---
+++ scripts.js
@@ -0,0 +1,11 @@
+  
+  
+  function  WhiteMode() {
+        document.body.style.backgroundColor= "white"
+        document.body.style.color= "black"
+    }
+    
+    function blackMode() {
+        document.body.style.backgroundColor= "black"
+        document.body.style.color= "white"
+   }
